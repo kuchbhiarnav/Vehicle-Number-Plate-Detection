0bcaef3e2168130722de238c719fb78a91d79be7
ARNAV RAJIV PATNAIK <77193445+kuchbhiarnav@users.noreply.github.com>
2023-10-03 07:07:31 +0200

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+# webserver gateway interface
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("layout.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
